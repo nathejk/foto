609efc69419de8c2a0e8ac317c6fadb022296939
Frederik Bjerggaard Nielsen <frederik@bjerggaard.me>
2023-08-25 19:38:42 +0200

    feat: Properly resize images
    Also adjusts filename according to #6

--- Controllers/PhotoController.cs
+++ Controllers/PhotoController.cs
@@ -1,7 +1,7 @@
-using System.Drawing;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Net.Http.Headers;
-using LazZiya.ImageResize;
+using SixLabors.ImageSharp.Metadata.Profiles.Exif;
+using SixLabors.ImageSharp.Metadata.Profiles.Iptc;
 
 namespace FotoApp.Controllers
 {
@@ -11,15 +11,21 @@ namespace FotoApp.Controllers
     {
         private readonly IConfiguration _configuration;
         private readonly IHttpContextAccessor _accessor;
+        private readonly ILogger<PhotoController> _logger;
 
-        public PhotoController(IConfiguration configuration, IHttpContextAccessor accessor)
+        public PhotoController(
+            IConfiguration configuration,
+            IHttpContextAccessor accessor,
+            ILogger<PhotoController> logger
+        )
         {
             _configuration = configuration;
             _accessor = accessor;
+            _logger = logger;
         }
 
         [HttpPost]
-        public IActionResult Upload([FromQuery] string teamNumber)
+        public async Task<IActionResult> Upload([FromQuery] string teamNumber)
         {
             try
             {
@@ -27,49 +33,72 @@ namespace FotoApp.Controllers
                 Directory.CreateDirectory(_configuration.GetValue<string>("PhotoPath") + "/fb");
                 var file = Request.Form.Files[0];
                 var pathToSave = _configuration["PhotoPath"];
-                if (file.Length > 0)
-                {
-                    //var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                    var extension = Path.GetExtension(
-                        ContentDispositionHeaderValue
-                            .Parse(file.ContentDisposition)
-                            .FileName?.Trim('"')
-                    );
-                    var fileName =
-                        $"{teamNumber}_{DateTime.UtcNow.ToString("yyyyMMdd_HHmmss")}{extension}";
-                    var fullPath = Path.Combine(pathToSave!, fileName);
-                    using (var stream = new FileStream(fullPath, FileMode.Create))
-                    {
-                        file.CopyTo(stream);
-
-                        try
-                        {
-                            using var img = Image.FromStream(stream);
-                            img.ScaleByHeightIf(img.Height >= img.Width, 2000)
-                                .ScaleByWidthIf(img.Width > img.Height, 2000)
-                                .SaveAs(
-                                    _configuration.GetValue<string>("PhotoPath") + "/fb/" + fileName
-                                );
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine(e);
-                        }
-                    }
-
-                    var hostUrl = new Uri(
-                        $"{_accessor.HttpContext?.Request.Scheme}://{_accessor.HttpContext?.Request.Host}/photos/{fileName}"
-                    );
-
-                    return new OkObjectResult(new { ImageUrl = hostUrl });
-                }
-                else
+                if (file.Length <= 0)
                 {
                     return BadRequest();
                 }
+
+                var extension = Path.GetExtension(
+                    ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName?.Trim('"')
+                );
+
+                var prefixCounter = 0;
+                string fileName;
+
+                do
+                {
+                    prefixCounter++;
+                    fileName = $"Team-{teamNumber}_{prefixCounter}{extension}";
+                    _logger.LogDebug("Incrementing prefixCounter to {Count}", prefixCounter);
+                } while (System.IO.File.Exists($"{pathToSave}/{fileName}"));
+
+                var fullPath = Path.Combine(pathToSave!, fileName);
+                await using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                try
+                {
+                    using var image = await Image.LoadAsync(fullPath);
+
+                    image.Metadata.IptcProfile ??= new IptcProfile();
+                    image.Metadata.IptcProfile.SetValue(IptcTag.BylineTitle, $"Hold {teamNumber}");
+                    image.Metadata.IptcProfile.SetValue(IptcTag.Headline, $"Hold {teamNumber}");
+                    image.Metadata.IptcProfile.SetValue(IptcTag.Name, $"Hold {teamNumber}");
+                    image.Metadata.IptcProfile.SetValue(IptcTag.Caption, $"Hold {teamNumber}");
+                    image.Metadata.IptcProfile.SetValue(
+                        IptcTag.CopyrightNotice,
+                        $"Nathejk {DateTime.Now.Year}"
+                    );
+
+                    await image.SaveAsync(fullPath);
+
+                    image.Mutate(
+                        x =>
+                            x.Resize(
+                                new ResizeOptions { Mode = ResizeMode.Max, Size = new Size(2000) }
+                            )
+                    );
+
+                    await image.SaveAsync(
+                        $"{_configuration.GetValue<string>("PhotoPath")}/fb/{fileName}"
+                    );
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "Failed resizing image");
+                }
+
+                var hostUrl = new Uri(
+                    $"{_accessor.HttpContext?.Request.Scheme}://{_accessor.HttpContext?.Request.Host}/photos/{fileName}"
+                );
+
+                return new OkObjectResult(new { ImageUrl = hostUrl });
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "Failed saving image!");
                 return StatusCode(500, $"Internal server error: {ex}");
             }
         }
@@ -78,7 +107,7 @@ namespace FotoApp.Controllers
         public IActionResult List()
         {
             var hostUrl = new Uri(
-                $"{_accessor.HttpContext.Request.Scheme}://{_accessor.HttpContext.Request.Host}"
+                $"{_accessor.HttpContext?.Request.Scheme}://{_accessor.HttpContext?.Request.Host}"
             );
             var localFiles = Directory.GetFiles(_configuration["PhotoPath"] + "/fb");
             var files = localFiles.Select(x => $"{hostUrl}photos/fb/{Path.GetFileName(x)}");
